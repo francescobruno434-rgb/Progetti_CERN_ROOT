a0b40c684af6256fc99bf592bb772bae15747ec3
Francesco Bruno <francesco.bruno434@edu.unito.it>
2026-03-19 10:01:50 +0100

    Add LAB_nucl_coeff_att function for attenuation analysis
    This file implements the LAB_nucl_coeff_att function to calculate and visualize attenuation coefficients using a graph.

---
+++ LAB_nucl_coeff_att.C
@@ -0,0 +1,82 @@
+#include<iostream>
+#include "TCanvas.h"
+#include "TGraphErrors.h"
+#include "TF1.h"
+#include <cmath
+#include<vector>
+
+using namespace std;
+
+
+//definizione di funzioni che saranno utili
+double err_rel_divisione(double err_num, double err_den){
+    double err_quad= pow(err_num, 2)+ pow(err_den, 2);
+    double err_rel= pow(err_quad, 0.5);
+    return err_rel;
+}
+
+
+
+
+void LAB_nucl_coeff_att(){
+
+    //++++++++++++++++++++++++++++++++++++++++++++++SCRIVERE NEI SEGUENTI VETTORI!! 
+    //vettore delle coincidenze
+    vector<double> rate= {0.31796963294187,
+        0.267486303987769,
+        0.212285350583223,
+        0.205904131905021,
+        0.217774547376768,
+        0.4543532809 //NB: questa è quella sul tetto
+         };
+    //vettore errore sul rate
+    vector<double> err_rate={0.0210217226114178,
+        0.0220484048715267,
+        0.0183298550087976,
+        0.0177114127365785,
+        0.0186546323932292,
+        0.02870389453
+    };
+    //vettore profondità CALCOLATA DALL'ALTO! il tetto è 0 per intendersi; la misura è in m (contano solo le solette)
+    vector<double> profondità={1.39,
+        2.32,
+
+        3.02,
+        3.7,
+        0   };
+    //
+    vector<double> err_profondità={0.014142135623731,
+        0.02,
+
+        0.0244948974278318,
+        0.0282842712474619,
+        0};
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++NON SCRIVERE PIU'!
+
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    TCanvas *c1= new TCanvas("c1", "Misura coeff attenuazione", 800, 600);
+    //
+    TGraphErrors* gr1= new TGraphErrors(profondità.data(), rate.data(), err_profondità.data(), err_rate.data() );
+    //
+    gr1->GetYaxis()->SetRangeUser(0, 0.6);
+    gr1->Draw("AP.");
+    gr1->Print("coeff_attenuazione.png", "png");
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
