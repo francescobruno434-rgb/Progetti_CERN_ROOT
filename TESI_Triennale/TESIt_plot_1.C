d8b0da7ea7631ca3af3df236f7b95d0a3aeac3be
Francesco Bruno <francesco.bruno434@edu.unito.it>
2026-03-25 19:20:55 +0100

    file riuscito bene per la prima analisi dati

--- TESI_Triennale/TESIt_plot_1.C
+++ TESI_Triennale/TESIt_plot_1.C
@@ -25,48 +25,142 @@ if(analisi->IsOpen()){
 //leggo il TTree
 TDirectoryFile* dir1= (TDirectoryFile*) analisi->Get("PWGLF_StrVsMult");
 TTree* tree= (TTree*) dir1->Get("fTreeCascade");
-//
-cout<<"Gli ingressi di tree sono "<<tree->GetEntries()<<endl;
-float massa=0; //creo la variabile massa in cui metterò i dati del tree
-//
-//Collego il Branch di mio interesse a questa variabile
-tree->SetBranchAddress("fTreeCascVarV0Mass", &massa);
-//
 //bisogna capire quante Entries (righe) ci sono; 
 int n= tree->GetEntries();
 //
-TCanvas* c1= new TCanvas("c1", "massa csi ", 800, 600);
-c1->Divide(1,2);
+cout<<"Gli ingressi di tree sono "<<n<<endl;
+//creo le variabili in cui metterò i dati del tree
+float mass_V0=0; 
+float mass_csi=0;
+float mass_omega=0;
+float mass_V0_lambda=0;
+float mass_V0_alambda=0;
+float DCA_casc=0;
+float DCA_V0=0;
+
 //
-TH1F* hist_normal= new TH1F("hist", "Plot massa", 2000, 1.107, 1.124); 
-//TH1F* hist_cut= new TH1F("hist", "Plot massa col cut", 50, 1.112, 1.125);
+//Collego il Branch di mio interesse a questa variabile
+tree->SetBranchAddress("fTreeCascVarV0Mass", &mass_V0);
+tree->SetBranchAddress("fTreeCascVarMassAsXi", &mass_csi);
+tree->SetBranchAddress("fTreeCascVarMassAsOmega", &mass_omega);
+tree->SetBranchAddress("fTreeCascVarV0MassLambda", &mass_V0_lambda);
+tree->SetBranchAddress("fTreeCascVarV0MassAntiLambda", &mass_V0_alambda);
+tree->SetBranchAddress("fTreeCascVarDCACascDaughters", &DCA_casc);
+tree->SetBranchAddress("fTreeCascVarDCAV0Daughters", &DCA_V0);
 //
-hist_normal->SetDirectory(0);
-//hist_cut->SetDirectory(0);
-float massa_min=pow(10, 15);
-float massa_max=0;
+//costruiamo il TCanvas dove vediamo le masse SENZA APPLICARE IL DCA
+//
+TCanvas* c1= new TCanvas("c1", "masse SENZA TAGLI", 1600, 1200);
+c1->Divide(2,2); //2 righe, 2 colonne
+//
+TCanvas* c2= new TCanvas("c2", "masse CON TAGLI", 1600, 1200);
+c2->Divide(2, 1);
+
+
+//qui mettiamo le variabili di minimo e massimo dei vari grafici
+//
+double min_V0=1.107;
+double max_V0= 1.124;
+//
+double min_csi= 1.3;
+double max_csi=1.35;
+//
+double min_omega= 1.6;
+double max_omega=1.690;
+//
+double min_lambda=1.1075;
+double max_lambda= 1.124;
+//
+//costruiamo i vari istogrammi di float 
+//
+//i seguenti sono per le masse senza tagli
+TH1F* hist_V0= new TH1F("hist_V0", "Plot massa V0", 2000, min_V0, max_V0); 
+TH1F* hist_csi= new TH1F("hist_csi", "plot massa csi", 1000, min_csi, max_csi); 
+TH1F* hist_omega= new TH1F("hist_omega", "plot massa omega", 2000, min_omega, max_omega);
+TH1F* hist_V0_lambda= new TH1F("hist_V0_lambda", "plot massa V0 lambda", 1000, min_lambda, max_lambda);
+TH1F* hist_V0_alambda= new TH1F("hist_V0_alambda", "plot massa anti lambda", 1000, min_lambda, max_lambda);
+//
+//i seguenti sono per le masse con tagli su DCA
+TH1F* c_hist_V0= new TH1F("c_hist_V0", "Plot massa V0", 1000, min_V0, max_V0); 
+TH1F* c_hist_csi= new TH1F("c_hist_csi", "plot massa csi", 1000, min_csi, max_csi); 
+TH1F* c_hist_omega= new TH1F("c_hist_omega", "plot massa omega", 1000, min_omega, max_omega);
+TH1F* c_hist_V0_lambda= new TH1F("c_hist_V0_lambda", "plot massa V0 lambda", 1000, min_lambda, max_lambda);
+TH1F* c_hist_V0_alambda= new TH1F("c_hist_V0_alambda", "plot massa anti lambda", 1000, min_lambda, max_lambda);
+//
+//questa parte qua serve per non far scomparire tutto 
+//
+hist_V0->SetDirectory(0);
+hist_csi->SetDirectory(0);
+hist_omega->SetDirectory(0);
+hist_V0_lambda->SetDirectory(0);
+hist_V0_alambda->SetDirectory(0);
+c_hist_V0->SetDirectory(0);
+c_hist_csi->SetDirectory(0);
+c_hist_omega->SetDirectory(0);
+c_hist_V0_lambda->SetDirectory(0);
+c_hist_V0_alambda->SetDirectory(0);
+//
+//ciclo che riempie le variabili
 for (int i=0; i<n; i++){
     tree->GetEntry(i);
-    if (massa<massa_min ){
-        massa_min=massa;
+  
+   //riempiamo gli istogrammi SOLA MASSA SENZA TAGLI
+    hist_V0->Fill(mass_V0);
+    if(min_csi<= mass_csi && mass_csi<=max_csi) hist_csi->Fill(mass_csi);
+     if(min_omega<= mass_omega && mass_omega<= max_omega)   hist_omega->Fill(mass_omega);
+    if(min_lambda<= mass_V0_lambda && mass_V0_lambda<=max_lambda)    hist_V0_lambda->Fill(mass_V0_lambda);
+    if(min_lambda<= mass_V0_alambda && mass_V0_alambda<= max_lambda)   hist_V0_alambda->Fill(mass_V0_alambda);
+    //
+    //riempiamo gli istogrammi che CONSIDERANO I TAGLI EFFETTUATI
+    if (DCA_casc<=0.1 && DCA_V0<=0.05) {
+        c_hist_csi->Fill(mass_csi);
+        c_hist_omega->Fill(mass_omega);
+        
     }
-    else if(massa> massa_max){
-        massa_max= massa;
-    }
-   
-    hist_normal->Fill(massa);
-    
-
-   /* if (massa>1.113 && massa<1.12){
-        hist_cut->Fill(massa);*/
-    
 }
+//ora facciamo i fit con le gaussiane
+TF1* fit_csi= new TF1("fit_csi", "gaus(0)+ pol3(3)", 1.3, 1.35);
+fit_csi->SetParameters(200, 1.322, 0.005, 400);
+
+//
+c1->cd(1);
+hist_V0->Rebin(7);
+hist_V0->Draw();
+hist_V0->SetTitle("massa V0");
+//
+c1->cd(2);
+hist_csi->Rebin(7);
+
+hist_csi->Draw();
+hist_csi->SetTitle("massa csi");
+//
+c1->cd(3);
+hist_omega->Draw();
+hist_omega->SetTitle("massa omega");
+//
+c1->cd(4);
+hist_V0_lambda->Rebin(7);
+hist_V0_alambda->Rebin(7);
+hist_V0_lambda->Draw();
+hist_V0_alambda->Draw("SAME");
+hist_V0_lambda->SetTitle("massa lambda");
+hist_V0_alambda->SetLineColor(kGreen);
 
 
-cout<<"la massa massima è "<<massa_max<<endl;
-cout<<"la massa minima è "<<massa_min<<endl;
+c2->cd(1);
+c_hist_csi->Rebin(10);
+c_hist_csi->Fit("fit_csi", "R");
+cout<<"il p value del fit massa csi è "<<fit_csi->GetProb()<<endl;
+c_hist_csi->SetTitle("massa csi");
+c_hist_csi->Draw();
 
-hist_normal->Draw("");
+c2->cd(2);
+c_hist_omega->Rebin(10);
+c_hist_omega->SetTitle("massa omega");
+c_hist_omega->Draw();
+
+
+
 
 
 
