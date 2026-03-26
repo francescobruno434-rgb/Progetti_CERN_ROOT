20a5ab6c973d029cea6c96ac03130f3545cb0177
Francesco Bruno <francesco.bruno434@edu.unito.it>
2026-03-26 12:55:15 +0100

    fit su 63.6 viene no quello su 87.5 no

---
+++ LAB_nucleare/LAB_nucl_TAC_63_6.C
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include "TH1D.h"
+#include "TF1.h"
+#include "TCanvas.h"
+#include<istream>
+using namespace std;
+
+
+
+void LAB_nucl_TAC_63_6(){
+    char skip_line[3000]; //array spazzatura vuoto, dove ficcheremo le linee da schippare
+
+
+    TCanvas* c1= new TCanvas("c1", "TCA con 63.6 cm di distanza", 800, 600);
+    TH1D* hist= new TH1D("hist", "gabibbi", 512, 0, 512);
+    //apro il file, che ora si chiama gabibbo;
+    fstream gabibbo("63_6_cent.mca", fstream::in);
+    if (gabibbo.is_open()){
+        cout<<"il file è correttamente aperto e gabibbo è contento"<<endl;
+    }
+    else {
+        cout<<"FERMATI!! IL FILE NON E' APERTO!! SEI PROPRIO UN GABIBBO"<<endl;
+    }
+    int i=0;
+    double value_bin=0;
+    while(gabibbo.eof()!=1 & i<526){
+        if(i<14){
+            gabibbo.getline(skip_line, 3000);
+            i++;
+        }
+        else{
+        gabibbo>> value_bin;
+        hist->SetBinContent(i-15, value_bin);
+        i++;
+    
+        //cout<<"bin aggiunto: "<<i<<endl;
+        //cout<<"valore inserito "<<value_bin<<endl;
+        }
+
+    }
+    TF1* fit= new TF1("fit", "gaus(0)+gaus(3)", 100, 300 ); //i parametri della gaussiana sono ampiezza, media e stdv (in ordine)
+    fit->SetParameters(150, 203, 25, 100, 225, 29 );
+   fit->SetParNames("A1", "mu1", "sigma 1", "A2", "mu2", "sigma 2");
+   fit->SetParLimits(4, 190, 250);
+   fit->SetParLimits(1, 180, 210);
+    hist->Rebin(7);
+    hist->Fit("fit", "R");
+    cout<<"il p-value è :"<<endl;
+    cout<<fit->GetProb()<<endl;
+    hist->Draw("");
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
