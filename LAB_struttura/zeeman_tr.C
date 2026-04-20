823459caf4338725eb3dc228ddcebc8fd2332f45
Francesco Bruno <francesco.bruno434@edu.unito.it>
2026-04-20 23:12:59 +0200

    modifiche B

---
+++ LAB_struttura/zeeman_tr.C
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <TGraphErrors.h>
+#include <TAxis.h>
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TH1.h>
+#include <iomanip>
+#include <TFitResultPtr.h>
+#include <TFitResult.h>
+#include <TLegend.h>
+#include <cmath>
+
+
+using namespace std;
+
+
+void zeeman_tr(){
+
+
+const int n=6;
+
+
+double B[n]={
+   9.689425,
+309.932425,
+371.181997,
+433.032055,
+490.078225,
+551.327797,
+};
+double D[n]={
+    0,
+    -0.140672299088437,
+    -0.171186258722491,
+    -0.199802167592311,
+    -0.224054328784307,
+    -0.243464059255216,
+    };
+double err_B[n]={
+    7.78612778501193,
+    44.9846399017394,
+    53.703538902043,
+    62.5390991147716,
+    70.7060712151463,
+    79.4875713881555,
+};
+double err_D[n]={
+        0,
+    0.0271805727632622,
+    0.0311826884803246,
+    0.051189494068092,
+    0.0405037693045907,
+    0.0405037693045907,
+};
+TCanvas *c1 = new TCanvas("c1", "verifica linearità", 800, 600);
+    c1->SetGrid();
+
+
+
+
+TGraphErrors *grafico = new TGraphErrors(n, B, D, err_B, err_D);
+    grafico->SetMarkerStyle(20);
+    grafico->SetMarkerSize(0.8);
+    grafico->SetMarkerColor(kBlack);
+    grafico->SetLineColor(kBlack);
+    grafico->Draw("AP");
+    grafico->SetTitle("<#delta>/<#Delta> vs B configurazione trasversale");
+    grafico->GetXaxis()->SetTitle("B [mT]");
+    grafico->GetYaxis()->SetTitle("<#delta>/<#Delta>");
+
+
+
+
+TF1 *fit1= new TF1("fit1", "[0]*x+[1]", -1, 650);
+    fit1->SetParLimits(0, -1, 0);
+    fit1->SetLineColor(8);
+    fit1->SetParameters(-0.003, 0);
+    grafico->Fit(fit1, "SR+");
+   
+     TFitResultPtr r = grafico->Fit(fit1, "S R Q");
+
+
+    // CALCOLO E STAMPA DEL COEFFICIENTE DI CORRELAZIONE LINEARE(r)
+    double R = grafico->GetCorrelationFactor();  
+
+
+     cout<<"il p-value è :"<<endl;
+    cout<<fit1->GetProb()<<endl;
+
+
+grafico->Draw("AP");
+fit1->Draw("same");
+c1->Print("zeeman_l.png", "png");
+
+}
+
